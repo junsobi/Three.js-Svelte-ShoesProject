c034cc777b5ba3107c44b3770577cbbeb32e0c1b
김준섭 <junsobi@gmail.com>
2023-06-21 11:12:11 +0900

    Add : gitigonre

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.e8a85e7a.js","_app/immutable/chunks/index.0a71bab1.js","_app/immutable/chunks/singletons.ecd5df17.js","_app/immutable/chunks/index.d626b966.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.f3f90d21.js","_app/immutable/chunks/index.0a71bab1.js","_app/immutable/chunks/index.d626b966.js"];
-export const stylesheets = ["_app/immutable/assets/2.db862fd0.css"];
-export const fonts = [];
