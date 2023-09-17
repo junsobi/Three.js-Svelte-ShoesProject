a8803bb629ef990ac2aa5af69bc76bda709ad8a0
junsobi <junsobi@gmail.com>
2023-09-17 17:41:29 +0900

    docs : svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter()
-// 	},
-// 	preprocess: vitePreprocess()
-// };
-// export default config;
-
-import adapter from "@sveltejs/adapter-static";
-
-export default {
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   kit: {
-    adapter: adapter({
-      fallback: "200.html", // may differ from host to host
-    }),
+    adapter: adapter(),
   },
+  preprocess: vitePreprocess(),
 };
+export default config;
+
+// import adapter from "@sveltejs/adapter-static";
+
+// export default {
+//   kit: {
+//     adapter: adapter({
+//       fallback: "200.html", // may differ from host to host
+//     }),
+//   },
+// };
